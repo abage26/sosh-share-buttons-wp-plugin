00f0e19c63ea08fafe59953fd59e8ff938e70d9c
abage26 <arth.bage@gmail.com>
2019-09-18 16:29:44 +0100

    fix bugs

--- sosh.php
+++ sosh.php
@@ -2,21 +2,25 @@
 /**
  * Plugin Name: Social Share
  * Description: Social share buttons plugin for <strong>share post to social network</strong>.
- * Version: 1.0
- * Author: KAMGOKO
- * Author URI: https://kamgoko.tech
+ * Version: 1.0.1
+ * Requires PHP: 5.4.0
+ * Author: Arthur AGOSSOU
+ *
  */
-session_start();
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 //session_unset();
 define('_PLUGIN_DIR', plugin_dir_path(__FILE__) );
 define('_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
 define('SOSH_OPTIONS_NAME', 'social_share_options_arth' );
 
-if( file_exists( _PLUGIN_DIR . '/inc/inc.php' ) )
+if (file_exists(_PLUGIN_DIR . '/inc/inc.php'))
     include_once (_PLUGIN_DIR . '/inc/inc.php');
 
-include_once( _PLUGIN_DIR . 'sosh.class.php' );
+if (file_exists(_PLUGIN_DIR . 'sosh.class.php'))
+    include_once( _PLUGIN_DIR . 'sosh.class.php' );
 new Social_share();
 
 register_activation_hook(__FILE__, ['Social_share','social_share_install']);
